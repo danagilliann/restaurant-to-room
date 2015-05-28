9b3f8ffe80fd9edde79ba22cc7ce5cf8b88d2a81
danagilliann <danalee96@gmail.com>
2015-05-28 05:50:02 +0000

    some cool login stuff

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,26 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// GET /users/view
-router.get('/view', function(req, res, next) {
-  res.send('user view');
+
+router.get('/create', function(req, res, next) {
+  var vm = {
+    title: 'Create an account'
+  };
+  res.render('users/create', vm);
+});
+
+router.post('/create', function(req, res, next) {
+  var somethingGoesWrong = true;
+  if (somethingGoesWrong) {
+    var vm = {
+      title: 'Create an account',
+      input: req.body,
+      error: 'Something went wrong'
+    };
+    delete vm.input.password;
+    return res.render('users/create', vm);
+  }
+  res.redirect('/orders');
 });
 
 module.exports = router;
