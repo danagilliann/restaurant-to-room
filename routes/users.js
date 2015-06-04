2ab1c85e61f80c57ee601e6724a9b30c287fac3b
danagilliann <danalee96@gmail.com>
2015-06-04 07:13:29 +0000

    mongo problems semi solved

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var userService = require('../services/user-service');
 
 /* GET users listing. */
@@ -30,6 +31,10 @@ router.post('/create', function(req, res, next) {
   }
   res.redirect('/orders');
  });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res, next) {
+  res.redirect('/orders');
 });
 
 module.exports = router;
